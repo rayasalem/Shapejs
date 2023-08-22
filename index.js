fd096afd7fb62cd4c51136bd4daaf8e50180e6aa
rayasalem <rayasalem483@gmail.com>
2023-08-22 16:02:46 +0300

    add arrow function

--- index.js
+++ index.js
@@ -3,48 +3,61 @@ const colorSelector = document.getElementById("colorselector");
 const generateBtn = document.getElementById("generateBtn");
 const clearBtn = document.getElementById("clearBtn");
 const shapeContainer = document.getElementById("shapeContainer");
+const textContainer=document.getElementById("text3");
+
+const shapes = {
+  circle: {
+    create: (element) => {
+      element.style.borderRadius = "50%";
+    },
+    applyColor: (element,colorSelector) => {
+      element.style.backgroundcolor=colorSelector;
+    }
+  },
+  square: {
+    create: (element) => {
+      element.style.width = "100px";
+      element.style.height = "100px";
+      element.style.borderRadius = "0";
+    },
+    applyColor: (element,colorSelector) => {
+      element.style.backgroundcolor=colorSelector;
+    }
+  },
+  triangle: {
+    create: (element) => {
+      element.style.width = "0";
+      element.style.height = "0";
+      element.style.borderLeft = "50px solid transparent";
+      element.style.borderRight = "50px solid transparent";
+      element.style.borderBottom = "100px solid ";
+      element.style.borderRadius = "0";
+    },
+    applyColor: (element,colorSelector) => {
+      element.style.color=colorSelector;
+    }
+  }
+};
 
 generateBtn.addEventListener("click", function () {
   const selectedShape = shapeSelector.value;
   const selectedColor = colorSelector.value;
+
   const newShape = document.createElement("div");
   newShape.className = "shape";
 
-  if (selectedShape === "circle") {
-    createCircle(newShape);
-  } else if (selectedShape === "square") {
-    createSquare(newShape);
-  }
-
-  if (selectedShape === "triangle") {
-     createTriangle(newShape);
-    newShape.style.color = selectedColor;
-  } else {
-    newShape.style.backgroundColor = selectedColor;
-  }
+  shapes[selectedShape].create(newShape);
+  shapes[selectedShape].applyColor(newShape, selectedColor);
 
   shapeContainer.innerHTML = "";
   shapeContainer.appendChild(newShape);
 });
+
 clearBtn.addEventListener("click", function () {
-  shapeContainer.innerHTML = "";
+  const shapeElement = shapeContainer.innerHTML;
+  const text=textContainer.innerHTML;
+  
+  if (shapeElement != textContainer) {
+    shapeContainer.innerHTML = "";
+  }
 });
-function createCircle(element) {
-  element.style.borderRadius = "50%";
-}
-function createSquare(element) {
-  element.style.width = "100px";
-  element.style.height = "100px";
-  element.style.borderRadius = "0";
-}
-function createTriangle(element) {
-  element.style.width = "0";
-  element.style.height = "0";
-  element.style.borderLeft = "50px solid transparent";
-  element.style.borderRight = "50px solid transparent";
-  element.style.borderBottom = "100px solid";
-  element.style.borderRadius = "0";
-}
-
-
-
